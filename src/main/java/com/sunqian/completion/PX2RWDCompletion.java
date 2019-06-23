9cf49f2dc775a3b4757e90dc06e00901106a5850
sunqian <sunqian1991@gmail.com>
2019-06-23 21:24:13 +0800

    添加了新的px2vw和px2vh代码自动完成提示，其中处理逻辑还没有加

--- src/main/java/com/sunqian/completion/PX2RWDCompletion.java
+++ src/main/java/com/sunqian/completion/PX2RWDCompletion.java
@@ -3,8 +3,12 @@ package com.sunqian.completion;
 import com.intellij.codeInsight.completion.*;
 import com.intellij.patterns.PlatformPatterns;
 import com.intellij.psi.css.impl.CssElementTypes;
+import com.sunqian.utils.LogicUtils;
+
+import java.util.Arrays;
 
 import static com.sunqian.constvalue.MagicValue.AUTO_COMPLETION_TAG;
+import static com.sunqian.constvalue.MagicValue.TO_RWD_TIPS;
 
 /**
  * 代码自动完成
@@ -17,7 +21,7 @@ public class PX2RWDCompletion extends CompletionContributor {
         extend(
                 CompletionType.BASIC,
                 PlatformPatterns.psiElement(CssElementTypes.CSS_IDENT).withParent(PlatformPatterns.psiElement(CssElementTypes.CSS_NUMBER_TERM)),
-                new PX2RWDProvider(AUTO_COMPLETION_TAG)
+                new PX2RWDProvider(LogicUtils.getLogic().generateObject(new String[TO_RWD_TIPS.length], tips -> Arrays.fill(tips, AUTO_COMPLETION_TAG)))
         );
     }
 }
--- src/main/java/com/sunqian/completion/PX2RWDProvider.java
+++ src/main/java/com/sunqian/completion/PX2RWDProvider.java
@@ -10,11 +10,17 @@ import com.sunqian.utils.LogicUtils;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static com.sunqian.constvalue.MagicValue.*;
 
 /**
  * 代码自动完成处理类
  */
+@SuppressWarnings("RedundantOperationOnEmptyContainer")
 public class PX2RWDProvider extends CompletionProvider<CompletionParameters> {
     private final String[] myItems;
 
@@ -24,14 +30,31 @@ public class PX2RWDProvider extends CompletionProvider<CompletionParameters> {
 
     @Override
     public void addCompletions(@NotNull CompletionParameters parameters, @NotNull ProcessingContext context, @NotNull CompletionResultSet result) {
-        Arrays.stream(myItems).forEach((LogicUtils.ExceptionConsumer<String>) item ->
-                result.addElement(LookupElementBuilder.create(item)
-                        .withCaseSensitivity(false).withTailText(" to rem/vw")
-                        .withInsertHandler(DIRECTIVE_HANDLER))
-        );
+        result.addAllElements(IntStream.range(0, myItems.length).mapToObj(i ->
+            LookupElementBuilder.create(myItems[i])
+                    .withCaseSensitivity(false).withTailText(TO_RWD_TIPS[i])
+                    .withInsertHandler(LogicUtils.getLogic().generateObject(new HashMap<String, InsertHandler<LookupElement>>(), map ->
+                            map.put(TO_REM_TIP, REM_HANDLER), map ->
+                            map.put(TO_VW_TIP, VW_HANDLER), map ->
+                            map.put(TO_VH_TIP, VH_HANDLER)).get(TO_RWD_TIPS[i]))).collect(Collectors.toList()));
     }
 
     private static final InsertHandler<LookupElement> DIRECTIVE_HANDLER = (context, item) ->
+            Optional.of(ActionPerformer.getActionPerformer(context.getProject(), context.getEditor())).ifPresent(actionPerformer ->
+                    FormatTools.getFormatTools(actionPerformer.getConstValue()).formatNearCode(actionPerformer)
+            );
+
+    private static final InsertHandler<LookupElement> REM_HANDLER = (context, item) ->
+            Optional.of(ActionPerformer.getActionPerformer(context.getProject(), context.getEditor())).ifPresent(actionPerformer ->
+                    FormatTools.getFormatTools(actionPerformer.getConstValue()).formatNearCode(actionPerformer)
+            );
+
+    private static final InsertHandler<LookupElement> VW_HANDLER = (context, item) ->
+            Optional.of(ActionPerformer.getActionPerformer(context.getProject(), context.getEditor())).ifPresent(actionPerformer ->
+                    FormatTools.getFormatTools(actionPerformer.getConstValue()).formatNearCode(actionPerformer)
+            );
+
+    private static final InsertHandler<LookupElement> VH_HANDLER = (context, item) ->
             Optional.of(ActionPerformer.getActionPerformer(context.getProject(), context.getEditor())).ifPresent(actionPerformer ->
                     FormatTools.getFormatTools(actionPerformer.getConstValue()).formatNearCode(actionPerformer)
             );
