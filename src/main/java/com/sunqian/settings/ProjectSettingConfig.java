6a7ed133957222273d774e7d85df664bad4beefa
sunqian <sunqian1991@gmail.com>
2019-06-23 21:22:54 +0800

    添加了新的设置页面，主要是将设置功能移动到settings页面中，同时添加了几个新的属性

---
+++ src/main/java/com/sunqian/settings/ProjectSettingConfig.java
@@ -0,0 +1,116 @@
+package com.sunqian.settings;
+
+import com.intellij.openapi.options.SearchableConfigurable;
+import com.intellij.openapi.project.Project;
+import com.sunqian.constvalue.ConstValue;
+import com.sunqian.constvalue.ShortCutType;
+import com.sunqian.utils.LogicUtils;
+import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.Optional;
+
+import static com.sunqian.constvalue.MagicValue.PLUGIN_NAME;
+
+@SuppressWarnings("RedundantOperationOnEmptyContainer")
+@Data
+public class ProjectSettingConfig implements SearchableConfigurable {
+
+    ProjectSettingsPage mainGui;
+
+    private Project project;
+    private ConstValue constValue;
+
+    public ProjectSettingConfig(@NotNull Project project) {
+        this.project = project;
+        constValue = ConstValue.getInstance(project);
+    }
+
+    @NotNull
+    @Override
+    public String getId() {
+        return getDisplayName();
+    }
+
+    @Nls(capitalization = Nls.Capitalization.Title)
+    @Override
+    public String getDisplayName() {
+        return PLUGIN_NAME;
+    }
+
+    @Nullable
+    @Override
+    public JComponent createComponent() {
+        mainGui = new ProjectSettingsPage(constValue);
+        return mainGui.getRootPanel();
+    }
+
+    @Override
+    public boolean isModified() {
+        return ifEdited();
+    }
+
+    @Nullable
+    @Override
+    public Runnable enableSearch(String option) {
+        return null;
+    }
+
+    @Override
+    public void disposeUIResources() {
+        mainGui = null;
+    }
+
+    @Override
+    public void reset() {
+        mainGui.getShowCalculationProcessInCheckBox().setSelected(constValue.getShowCalculationProcess());
+        mainGui.getRemRadioButton().setSelected(constValue.getShortCutType() == ShortCutType.REM);
+        mainGui.getVwRadioButton().setSelected(constValue.getShortCutType() == ShortCutType.VW);
+        mainGui.getVhRadioButton().setSelected(constValue.getShortCutType() == ShortCutType.VH);
+        mainGui.getRemBaseValue().setText(constValue.getRemBaseValue() + "");
+        mainGui.getVwValue().setText(constValue.getWidthValue().toString());
+        mainGui.getVhValue().setText(constValue.getHeightValue().toString());
+    }
+
+    @Override
+    public void apply() {
+        LogicUtils.getLogic().conOrEnd(mainGui.getRemBaseValue().getText(), text -> Objects.nonNull(text) && NumberUtils.isNumber(mainGui.getRemBaseValue().getText()), text -> constValue.setRemBaseValue(mainGui.getRemBaseValue().getText()));
+        constValue.setShowCalculationProcess(mainGui.getShowCalculationProcessInCheckBox().isSelected());
+        LogicUtils.getLogic().conOrEnd(mainGui.getVwValue().getText(), text -> Objects.nonNull(text) && NumberUtils.isNumber(text), text -> constValue.setWidthValue(mainGui.getVwValue().getText()));
+        LogicUtils.getLogic().conOrEnd(mainGui.getVhValue().getText(), text -> Objects.nonNull(text) && NumberUtils.isNumber(text), text -> constValue.setHeightValue(mainGui.getVhValue().getText()));
+        constValue.setShortCutType(getShortCutType());
+    }
+
+    private ShortCutType getShortCutType() {
+        return Optional.ofNullable(LogicUtils.getLogic().generateObject(new HashMap<String, ShortCutType>(), map ->
+                map.put("rem", ShortCutType.REM), map ->
+                map.put("vw", ShortCutType.VW), map ->
+                map.put("vh", ShortCutType.VH)).get(StringUtils.join(
+                mainGui.getRemRadioButton().isSelected() ? "rem" : "",
+                mainGui.getVwRadioButton().isSelected() ? "vw" : "",
+                mainGui.getVhRadioButton().isSelected() ? "vh" : ""
+        ))).orElse(ShortCutType.REM);
+    }
+
+    private boolean ifEdited() {
+        return LogicUtils.getLogic().or(flag -> !Objects.equals(flag[0], flag[1]), new Double[]{
+                constValue.getRemBaseValue(), NumberUtils.toDouble(mainGui.getRemBaseValue().getText())
+        }, new Double[]{
+                constValue.getWidthValue(), NumberUtils.toDouble(mainGui.getVwValue().getText())
+        }, new Double[]{
+                constValue.getHeightValue(), NumberUtils.toDouble(mainGui.getVhValue().getText())
+        }, new ShortCutType[]{
+                constValue.getShortCutType(), getShortCutType()
+        }, new Boolean[]{
+                constValue.getShowCalculationProcess(), mainGui.getShowCalculationProcessInCheckBox().isSelected()
+        });
+    }
+}
